26e42d2a11c6d4721dc23fe25d3322bef649350d
shithi <shithisarker20@gmail.com>
2024-10-22 01:03:54 +0600

    added

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+
+
+import './App.css'
+import Header from './components/Header/Header'
+import Blogs from './components/Blogs/Blogs'
+
+function App() {
+
+  return (
+    <>
+     
+     <Header></Header>
+     <Blogs></Blogs>
+      
+      
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            
+      <h1 className='text-3xl'>React Knowladge cafe</h1>
+        </div>
+    );
+};
+
+export default Header;
